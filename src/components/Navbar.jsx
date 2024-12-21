d602155107401e580a3e2af78d736914e8bb1cd9
Kapil Motwani <kapilmotwani1010@gmail.com>
2024-12-21 12:24:18 +0530

    navbar desktop view proper

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,18 +5,11 @@ import { NavLink } from "react-router-dom";
 export default function Navbar() {
   const [isMenuHidden, setIsMenuHidden] = useState(true);
   return (
-    <nav className="mb-10 flex flex-col items-center justify-between gap-10 px-10 pt-5 md:flex-row">
-      <section className="flex w-full justify-between">
+    <nav className="mb-10 flex flex-col items-center justify-between px-10 pt-5 md:flex-row">
+      <section className="flex w-full items-center justify-between">
         <img src="/brand.png" className="h-7 object-contain" />
-        <button
-          className="md:hidden"
-          onClick={() => setIsMenuHidden((prev) => !prev)}
-        >
-          <Menu />
-        </button>
-      </section>
-      {!isMenuHidden && (
-        <section className="flex flex-1 flex-col items-center justify-between gap-10 md:flex-row md:gap-0">
+
+        <section className="flex items-center justify-between gap-5">
           <ul className="flex flex-col gap-5 md:flex-row">
             <li>
               <NavLink to="/">Home</NavLink>
@@ -32,27 +25,34 @@ export default function Navbar() {
             </li>
           </ul>
 
-          <ul className="flex items-center gap-5">
-            <section className="relative">
-              <input
-                type="text"
-                placeholder="What are you looking for?"
-                className="w-[110%] py-3 pr-10 ps-3"
-              />
-              <article className="pointer-events-none absolute inset-y-0 right-2 flex items-center pl-3">
-                <Search color="#aaa" size="20" />
-              </article>
-            </section>
+          <section className="relative mr-5">
+            <input
+              type="text"
+              placeholder="What are you looking for?"
+              className="w-[110%] py-3 pr-10 ps-3"
+            />
+            <article className="pointer-events-none absolute inset-y-0 right-2 flex items-center pl-3">
+              <Search color="#aaa" size="20" />
+            </article>
+          </section>
 
+          <section className="flex items-center justify-between gap-5">
             <NavLink to="wishlist">
               <Heart />
             </NavLink>
             <NavLink to="cart">
               <ShoppingCartIcon />
             </NavLink>
-          </ul>
+          </section>
+
+          <button
+            className="md:hidden"
+            onClick={() => setIsMenuHidden((prev) => !prev)}
+          >
+            <Menu />
+          </button>
         </section>
-      )}
+      </section>
     </nav>
   );
 }
